cb201904d0c003f50fd122605ee36cdfbf9a58be
pulipatirohithkumar74-wq <pulipatirohithkumar74@gmail.com>
2025-12-20 22:48:55 +0530

    Add files via upload

---
+++ 20-12-2025/program 4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    //Your Code goes here!
+    int age;
+    scanf("%d",&age);
+    if(age>=18){
+        printf("eligible for vote");
+    }
+    else{
+        printf("not eligible for vote");
+    }
+    
+    return 0;
+}
---
+++ 20-12-2025/program 5.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    //Your Code goes here!
+    int marks;
+    scanf("%d",&marks);
+    if(marks>=40){
+        printf("passed");
+    }
+    else{
+        printf("failed");
+    }
+    
+    return 0;
+}
