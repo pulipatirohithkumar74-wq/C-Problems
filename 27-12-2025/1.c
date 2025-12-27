18c8b4f912dc90306a4ea01e469986f042cfbe8b
pulipatirohithkumar74-wq <pulipatirohithkumar74@gmail.com>
2025-12-27 19:39:45 +0530

    Add files via upload

---
+++ 27-12-2025/1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    //Your Code goes here!
+    int a;
+    scanf("%d",&a);
+    if(a%2==0){
+        printf("even");
+    }
+    else if(a%2!=0){
+        printf("Odd");
+    }
+    return 0;
+}
