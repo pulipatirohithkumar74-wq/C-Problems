4ffafc06919fa97bb6988cb3c0a3e6f593becb3b
pulipatirohithkumar74-wq <pulipatirohithkumar74@gmail.com>
2026-01-03 23:03:18 +0530

    Add files via upload

---
+++ 3.01.2026/1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main() {
+    //Your Code goes here!
+    int n,a=0,b=1,c;
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++){
+        if(i==1){
+            printf("%d ",a);
+        }
+        else if(i==2){
+            printf("%d ",b);
+        }
+        else{
+            c=a+b;
+            printf("%d ",c);
+            a=b;
+            b=c;
+
+        }
+    }
+    return 0;
+}
