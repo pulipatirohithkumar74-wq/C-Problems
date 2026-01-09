f261711f2877c5d7243a35cd1fdbc0305888d8cb
pulipatirohithkumar74-wq <pulipatirohithkumar74@gmail.com>
2026-01-09 23:19:24 +0530

    Add files via upload

---
+++ 9.12.2026/47.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str1[100], str2[100];
+    scanf("%s", str1);
+
+    strcpy(str2, str1);
+
+    printf("%s", str2);
+    return 0;
+}
