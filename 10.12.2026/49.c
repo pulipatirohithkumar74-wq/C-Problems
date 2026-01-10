94f76b1fd9fffce25b76684ac0a7b7b10a328e9a
pulipatirohithkumar74-wq <pulipatirohithkumar74@gmail.com>
2026-01-10 22:42:04 +0530

    Add files via upload

---
+++ 10.12.2026/49.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str1[100], str2[100];
+    scanf("%s %s", str1, str2);
+
+    if (strcmp(str1, str2) == 0)
+        printf("Equal");
+    else
+        printf("Not Equal");
+
+    return 0;
+}
