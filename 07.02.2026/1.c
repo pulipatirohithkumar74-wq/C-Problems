262ee60d7933b91174ed3b2c976fc6727742fa9e
pulipatirohithkumar74-wq <pulipatirohithkumar74@gmail.com>
2026-02-07 20:57:04 +0530

    Add files via upload

---
+++ 07.02.2026/1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+
+    printf("Before swap (by reference): a = %d, b = %d\n", a, b);
+    swap(&a, &b);
+    printf("After swap (by reference): a = %d, b = %d", a, b);
+
+    return 0;
+}
