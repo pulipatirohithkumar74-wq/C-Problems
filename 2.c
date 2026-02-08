0cacf2ebce2611eb422ac2bdf5240acf49606f5f
pulipatirohithkumar74-wq <pulipatirohithkumar74@gmail.com>
2026-02-08 21:01:51 +0530

    Add files via upload

--- 2.c
+++ 2.c
@@ -1,18 +1,11 @@
 #include <stdio.h>
 
 int main() {
-    char str[50];
-    int i, len = 0;
+    char msg[100];
 
-    scanf("%s", str);
+    fgets(msg, sizeof(msg), stdin);
 
-    while (str[len] != '\0') {
-        len++;
-    }
-
-    for (i = len - 1; i >= 0; i--) {
-        printf("%c", str[i]);
-    }
+    printf("%s", msg);
 
     return 0;
 }
