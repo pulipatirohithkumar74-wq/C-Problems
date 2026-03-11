0ec2a304e0421883fb364ec8cfbe260b86fbe38e
pulipatirohithkumar74-wq <pulipatirohithkumar74@gmail.com>
2026-03-11 21:07:59 +0530

    Create loop basic.c

---
+++ loop basic.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    //Your Code goes here!
+    int n;
+    scanf("%d",&n);
+    for(int i=0;i<=n;i++){
+        printf("I KNOW LOOPS \n");
+    }
+    return 0;
+}
