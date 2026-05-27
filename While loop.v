0294bd45489d175558815b40d4e253741af0d9e3
pulipatirohithkumar74-wq <pulipatirohithkumar74@gmail.com>
2026-05-27 08:25:39 +0530

    Implement while loop to calculate sum of inputs

---
+++ While loop.v
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, sum = 0;
+
+    while(1)
+    {
+        scanf("%d",&n);
+
+        if(n == 0)
+            break;
+
+        sum += n;
+    }
+
+    printf("Sum = %d",sum);
+
+    return 0;
+}
